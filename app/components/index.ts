126b3876495b8c5d0190df713ae6114269390201
francis <FrancisMingomba>
2024-12-08 08:19:52 +0000

    Initial commit

---
+++ app/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Link } from './Links';
+export { default as ErrorMessage } from './ErrorMessage';
+export { default as TaskStatusBadge } from './taskStatusBadge';
+export { default as Spinner } from './Spinner';
+export { default as Skeleton } from './Skeleton';
---
+++ app/tasks/[id]/_components/edit/loading.tsx
@@ -0,0 +1,2 @@
+import IssueFormSkeleton from "../../_components/TaskFormSkeleton";
+export default IssueFormSkeleton;
